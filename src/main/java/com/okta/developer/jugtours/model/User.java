3c9397a660d939f90eeb3a1b1253e32bcb9966f0
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-05-14 09:54:22 +0800

    Created models

---
+++ src/main/java/com/okta/developer/jugtours/model/User.java
@@ -0,0 +1,20 @@
+package com.okta.developer.jugtours.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private long id;
+    private String name;
+    private  String email;
+}
